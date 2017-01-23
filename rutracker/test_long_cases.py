2771cc3b977be4513b076a2189a3a04b952da3b0
Timur Nurlygayanov <tnurlygayanov@mirantis.com>
2017-01-23 18:10:06 +0300

    Skipped long test case

--- rutracker/test_long_cases.py
+++ rutracker/test_long_cases.py
@@ -1,48 +1,65 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
+# TODO: identify the way to properly filter
+#       forums without attached torrents,        
+#       because get_forum method returns 404
+#       status code if forum exists but doesn't have
+#       attached torrent files
+
+import pytest
+import requests
 from joblib import Parallel, delayed
+
+from test_variables import ENDPOINTS
+
+
+IGNORE_FORUMS = [u'f.a.q.', u'faq', u'архив', u'предложения',
+                 u'ремонт и эксплуатация tc', u'общени',
+                 u'помощь по разделу', u'ищу']
+
 
 def check_status_code200(url):
     r = requests.get(url)
     assert r.status_code == 200, "URL: " + url
 
 
+def check_forum_title(title):
+    for w in IGNORE_FORUMS:
+        if w.lower() in title.lower():
+            return False
+    return True
+
+
+@pytest.mark.long
+@pytest.mark.xfail(run=False)
 def test_check_get_each_forum():
     """ Test checks that we can get any forum from the list of forums. """
 
-    r = requests.get('http://api.rutracker.org/v1/static/cat_forum_tree')
+    # get list of all forums:
+    r = requests.get(ENDPOINTS['cat_forum_tree'])
     data = r.json()
     forums_titles = data['result']['f']
-    errors = []
 
     categories_without_torrents = [u'Обсуждения, встречи, общение',
                                    u'Новости',
                                    u'Вопросы по форуму и трекеру',
                                    u'ОБХОД БЛОКИРОВОК']
-    ignore_forums = [u'F.A.Q.', u'Архив', u'Ремонт и эксплуатация ТС',
-                     u'Общение', u'Помощь по разделу']
 
     forums = []
     categories = data['result']['tree']
-    for category in data['result']['tree']:
+
+    # collect all forums with attached torrents:
+    for category in categories:
         if categories[category] not in categories_without_torrents:
-            for f in data['result']['tree'][category].keys():
-                forum_without_torrents = False
-                for title in ignore_forums:
-                    if title in forums_titles[f]:
-                        forum_without_torrents = True
-                if not forum_without_torrents:
-                    forums.append(f)
-                    for forum in data['result']['tree'][category]:
-                        for f in data['result']['tree'][category][forum]:
-                            forum_without_torrents = False
-                            for title in ignore_forums:
-                                if title in forums_titles[str(f)]:
-                                    forum_without_torrents = True
+            for forum in categories[category].keys():
+                if check_forum_title(forums_titles[forum]):
+                    forums.append(forum)
+                    for subforum in categories[category][forum]:
+                        if check_forum_title(forums_titles[str(subforum)]):
+                            forums.append(subforum)
 
-                            if not forum_without_torrents:
-                                forums.append(f)
-
-    url = 'http://api.rutracker.org/v1/static/pvc/f/{0}'
-    Parallel(n_jobs=1000)(delayed(check_status_code200)(url.format(forum)) for forum in forums)
+    # get every torrent from the list and make sure it exists:
+    Parallel(n_jobs=1000)(delayed(check_status_code200)
+                          (ENDPOINTS['get_forum'].format(forum))
+                          for forum in forums)
